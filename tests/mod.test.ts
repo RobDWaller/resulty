e8505ddca0e42ceb8364c71c33fac22bb9797cd5
RobDWaller <rdwaller1984@googlemail.com>
2020-05-20 13:19:31 +0100

    Wrote firt test for core module.

---
+++ tests/mod.test.ts
@@ -0,0 +1,28 @@
+import { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { ok, Result, some, Err, err, none, Maybe, maybe } from "../mod.ts";
+
+Deno.test("Result Ok String", () => {
+  let resultOk: Result<string> = ok<string>("Hello");
+
+  assert(!resultOk.isError());
+  assertEquals(resultOk.unwrap(), "Hello");
+});
+
+Deno.test("Result Error Some", () => {
+  let resultError: Result<Err> = err(some("Fail!"));
+
+  assert(resultError.isError());
+  assertEquals(resultError.unwrap(), "Fail!");
+});
+
+Deno.test("Result Error None", () => {
+  let resultError: Result<Err> = err(none);
+
+  assert(resultError.isError());
+});
+
+Deno.test("Maybe Some", () => {
+  let maybeSome: Maybe = maybe(some("Hello!"));
+
+  assertEquals(maybeSome.unwrap(), "Hello!");
+});
