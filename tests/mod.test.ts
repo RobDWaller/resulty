c1b76824ce60169036178916255dd71290e38013
RobDWaller <rdwaller1984@googlemail.com>
2020-05-29 09:34:37 +0100

    Created tests for the example shown in the README to ensure they work.

--- tests/mod.test.ts
+++ tests/mod.test.ts
@@ -128,3 +128,69 @@ Deno.test("Result Err is not ok", () => {
 
   assertStrictEq(result.isOk(), false);
 });
+
+Deno.test("Generic Ok Example", () => {
+  const isOk: Result<string> = ok("Hello");
+
+  assertStrictEq(isOk.unwrap(), "Hello");
+});
+
+Deno.test("Result Example One", () => {
+  let isSandra = function (name: string): Result<string> {
+    if (name === "Sandra") {
+      return ok("Is Sandra");
+    }
+    return err("Is not Sandra");
+  };
+  
+  let geoff = isSandra("Geoff");
+  
+  assertStrictEq(geoff.unwrap(), "Is not Sandra");
+
+  let sandra = isSandra("Sandra");
+  
+  assertStrictEq(sandra.unwrap(), "Is Sandra");
+});
+
+Deno.test("Result Example Two", () => {
+  let findNumber = function (toFind: number): Result<number | string> {
+    let numbers = [1, 4, 6, 7, 21, 33];
+    
+    if (numbers.includes(toFind)) {
+      return ok(toFind);
+    }
+    return err(`Number: ${toFind} could not be found.`);
+  };
+  
+  let found = findNumber(6);
+  
+  assertStrictEq(found.unwrap(), 6);
+
+  let notFound = findNumber(9);
+
+  assertStrictEq(notFound.unwrap(), "Number: 9 could not be found.");
+});
+
+Deno.test("Option Example One", () => {
+  let findRecord = function (id: number): Opt<string> {
+    let records = [{id: 1, value: "Hello"}, {id: 13, value: "World"}];
+    
+    records = records.filter((item) => {
+      return item.id === id;
+    });
+
+    if (records.length === 1) {
+      return some(records[0].value);
+    }
+
+    return none();
+  };
+  
+  let found = findRecord(13);
+  
+  assertStrictEq(found.unwrap(), "World");
+
+  let notFound = findRecord(2);
+
+  assertStrictEq(notFound.unwrap(), null);
+});
