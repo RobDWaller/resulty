51ae2a5ca8456d1ac8b56f67e884949f80237c80
RobDWaller <rdwaller1984@gmail.com>
2020-06-25 11:01:54 +0100

    Created Panic Error class, for use in Options and Result objects.

---
+++ tests/panic.test.ts
@@ -0,0 +1,19 @@
+import {
+  assertStrictEq,
+  assertThrows,
+} from "https://deno.land/std@0.56.0/testing/asserts.ts";
+import { Panic } from "../src/panic.ts";
+
+Deno.test("Instance of Panic", () => {
+  const panic = new Panic("Oh no!");
+
+  assertStrictEq(panic instanceof Panic, true);
+  assertStrictEq(panic instanceof Error, true);
+  assertStrictEq(panic.name, "Panic");
+});
+
+Deno.test("Throws Panic", () => {
+  assertThrows(() => {
+    throw new Panic("Oh no!");
+  }, Panic, "Oh no!");
+});
